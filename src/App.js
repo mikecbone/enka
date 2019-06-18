6acea17b100df5b39248dd401a5b8fc98866191f
Mike <mike@defproc.co.uk>
2019-06-18 15:55:26 +0100

    Added unstyled colour level slider

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import seedColors from './seedColors';
 import generatePalette from './colourHelpers';
 
 function App() {
-  console.log(generatePalette(seedColors[4]));
   return (
     <div>
-      <Palette {...seedColors[4]}/>
+      <Palette palette={generatePalette(seedColors[4])}/>
     </div>
   );
 }
