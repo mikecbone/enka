8f2e13c0b2530173c8265050bb5d2dac1bae836f
Mike <mike@defproc.co.uk>
2019-06-19 14:23:52 +0100

    Added palette routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import generatePalette from './colourHelpers';
+import { Route, Switch } from 'react-router-dom';
+import { palette } from '@material-ui/system';
 
 function App() {
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])}/>
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>TEST</h1>}/>
+      <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))}/>}/>
+    </Switch>
   );
+}
+
+function findPalette(id){
+  return seedColors.find(function(palette) {
+    return palette.id === id;
+  })
 }
 
 export default App;
